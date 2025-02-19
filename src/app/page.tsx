28dcb3a1113599a2db4c808048b3e904da0582f7
nathancamolez-dev <nathan.camolez@unesp.br>
2025-02-19 20:03:27 -0300

    feat: Assets an first steps of home page
    First configuration of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image'
+import logo from '../assets/logo.svg'
+
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
+    <main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0">
+      <div className="min-h-dvh flex flex-col  justify-center gap-16">
+        <div className="flex flex-col gap-8 items-center md:items-start">
+          <Image src={logo} alt="devstage" width={108.2} height={30} />
+
+          <h1 className="text-4xl text-center leading-none font-heading font-medium flex flex-col md:text-7xl md:text-left">
+            <span className="text-blue">CodeCraft</span>
+          </h1>
+        </div>
+      </div>
     </main>
-  );
+  )
 }
--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,14 +1,14 @@
-import type { ComponentProps } from "react";
+import type { ComponentProps } from 'react'
 
-interface IconButtonProps extends ComponentProps<"button"> {}
+interface IconButtonProps extends ComponentProps<'button'> {}
 
 export function IconButton(props: IconButtonProps) {
-	return (
-		<main>
-			<button
-				className=" px-1.5 bg-gray-500 text-blue  rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
-				{...props}
-			/>
-		</main>
-	);
+  return (
+    <main>
+      <button
+        className=" px-1.5 bg-gray-500 text-blue  rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
+        {...props}
+      />
+    </main>
+  )
 }
