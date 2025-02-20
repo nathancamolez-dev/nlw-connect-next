1f75ba380a0a7c5d2e575b358232579d9218bd14
nathancamolez-dev <nathan.camolez@unesp.br>
2025-02-20 07:03:38 -0300

    feat: refactoring and functions of form and invite page
    Splited all code than can be maded as component, done  functions as copy
    referral link and form validation

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import logo from '../assets/logo.svg'
+import logo from '../../assets/logo.svg'
 import { SubscriptionForm } from './subscription-form'
 
 export default function Home() {
--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -6,6 +6,7 @@ import { Ranking } from './ranking'
 import { Stats } from './stats'
 
 export default function InvitePage() {
+  const inviteLink = 'http://localhost:3000/invite/1234123'
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -30,7 +31,7 @@ export default function InvitePage() {
           </div>
         </div>
 
-        <InviteLinkInput />
+        <InviteLinkInput inviteLink={inviteLink} />
 
         <Stats />
       </div>
